21ffab62cc41badbc01b44f4c21d563c4e668595
chandrahaswtw <chandrahaswtw@gmail.com>
2021-07-19 17:30:05 +0530

    Prefix classnames

--- packages/container/src/app.js
+++ packages/container/src/app.js
@@ -2,6 +2,14 @@ import React, { useEffect, useRef } from "react";
 import { mount as marketingMount } from "MarketingHost/showMarketing";
 import Header from "./Header";
 import { BrowserRouter } from "react-router-dom";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 
 const App = () => {
   const ref = useRef(null);
@@ -11,12 +19,14 @@ const App = () => {
   }, []);
 
   return (
-    <BrowserRouter>
-      <div>
-        <Header></Header>
-        <div ref={ref}></div>
-      </div>
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div>
+          <Header></Header>
+          <div ref={ref}></div>
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
 
